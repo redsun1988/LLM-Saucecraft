193ee06628ebf3e8b4e1ffd521d3f0bfbfa85af4
Konyaev Maksim <redsun1988@rambler.ru>
2025-08-25 04:33:09 +0300

    fixed chat generation code

--- managers/OllamaClient.py
+++ managers/OllamaClient.py
@@ -8,21 +8,25 @@ class OllamaClient:
         self.model = model
         self.client = ollama.Client()
 
-    def chat(self, prompt: str, format_model: Optional[BaseModel] = None) -> Any:
+    def chat(self, prompt: str, format_model: Optional[BaseModel] = None, think=True, options=None) -> Any:
         """
         Отправляет запрос в LLM и парсит ответ, используя Pydantic модель.
         Обратите внимание: `ollama.chat` напрямую не принимает Pydantic BaseModel в `format`.
         Мы указываем `format='json'` и инструктируем LLM в промпте генерировать JSON,
         соответствующий Pydantic модели.
         """
+        # response_format = 'json' if format_model else 'text'
+        # response_text = ""
+        # try:
         messages = [{"role": "user", "content": prompt}]
+        # if format_model:
+        #     messages["content"] += (
+        #         f"\n\nПожалуйста, сгенерируйте ответ строго в формате JSON, "
+        #         f"соответствующем следующей Pydantic схеме: {format_model.model_json_schema(indent=2)}"
+        #     )
 
         if format_model:
-            messages["content"] += (
-                f"\n\nПожалуйста, сгенерируйте ответ строго в формате JSON, "
-                f"соответствующем следующей Pydantic схеме: {format_model.model_json_schema(indent=2)}"
-            )
-            response_text = self.client.chat(model=self.model, messages=messages, format=format_model.model_json_schema()).message.content
+            response_text = self.client.chat(model=self.model, messages=messages, format=format_model.model_json_schema(), think=think, options=options).message.content
             return format_model.model_validate_json(response_text)
         else:
             response_text = self.client.chat(model=self.model, messages=messages).message.content
